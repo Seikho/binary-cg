cdd8f0a198f69a6e0aca7fc1017735ffbb4aae8a
Carl Winkler <carl.winkler86@gmail.com>
2015-10-13 19:06:24 +0800

    Implement game end recognition

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 var CG = require('../index.d.ts');
+var Enum = require('./enums');
 var obs = require('observers');
+var makeDeck = require('./makeDeck');
+exports.Command = Enum.Command;
+exports.Player = Enum.Player;
+exports.Direction = Enum.Direction;
 var Game = (function () {
     function Game() {
-        this.turn = obs.observe(0 /* Zero */);
+        var _this = this;
+        this.turn = obs.observe(CG.Player.Zero);
         this.shuffle = function (deck) { return deck.cards.sort(function () { return Math.random() > Math.random() ? 1 : -1; }); };
         this.discarded = obs.observeArray([]);
+        this.start = function () {
+            var commands = Object.keys(exports.Command)
+                .filter(function (key) { return isNaN(exports.Command[key]); })
+                .map(function (value) { return Number(value); });
+            var decks = makeDeck(commands);
+            _this.oneDeck = decks.one;
+            _this.zeroDeck = decks.zero;
+            _this.grid = {
+                Ten: obs.observeArray([]),
+                Twenty: obs.observeArray([]),
+                Thirty: obs.observeArray([]),
+                Forty: obs.observeArray([]),
+                Fifty: obs.observeArray([]),
+            };
+            var noCards = function (cards) { return cards.length === 0 ? _this.endGame() : void 0; };
+            var rowFiftyFull = function (cards) { return cards.length === 3; };
+            _this.endGame();
+            void 0;
+            _this.oneDeck.cards.subscribe(noCards);
+            _this.zeroDeck.cards.subscribe(noCards);
+            _this.grid.Fifty.subscribe(rowFiftyFull);
+        };
+        this.endGame = function () { };
     }
     return Game;
 })();
+exports.Game = Game;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,49 @@
 import * as CG from '../index.d.ts';
+import Enum = require('./enums');
 import obs = require('observers');
+import makeDeck = require('./makeDeck');
 
-class Game {
-	constructor() {}
-	
+export import Command = Enum.Command;
+export import Player = Enum.Player;
+export import Direction = Enum.Direction;
+
+export class Game {
+	constructor() {
+		
+	 }
+
 	turn = obs.observe(CG.Player.Zero);
-	shuffle = (deck: CG.Deck) => deck.cards.sort(() => Math.random() > Math.random() ? 1 : -1);	
-	discarded = obs.observeArray<CG.Card>([]);
+	shuffle = (deck: CG.Deck) => deck.cards.sort(() => Math.random() > Math.random() ? 1 : -1);
+	discarded = obs.observeArray<CG.PlayerCard>([]);
+
+	oneDeck: CG.Deck;
+	zeroDeck: CG.Deck;
+	grid: CG.Grid;
+
+	start = () => {
+		var commands = Object.keys(Command)
+			.filter(key => isNaN(Command[key]))
+			.map(value => Number(value));
+		var decks = makeDeck(commands);
+		this.oneDeck = decks.one;
+		this.zeroDeck = decks.zero;
+		this.grid = {
+			Ten: obs.observeArray([]),
+			Twenty: obs.observeArray([]),
+			Thirty: obs.observeArray([]),
+			Forty: obs.observeArray([]),
+			Fifty: obs.observeArray([]),
+		};
+		
+		var noCards = (cards: CG.PlayerCard[]) => cards.length === 0 ? this.endGame() : void 0;
+		var rowFiftyFull = (cards: CG.PlayerCardp[] => cards.length === 3) ? this.endGame() : void 0;
+		this.oneDeck.cards.subscribe(noCards)
+		this.zeroDeck.cards.subscribe(noCards);
+		this.grid.Fifty.subscribe(rowFiftyFull);
+	}
+	
+	endGame = () => {};
+	
+	
+	
 }
